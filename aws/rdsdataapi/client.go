618a3606482fee1cc7b51ff6a7a8b76bc0a46fb9
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2022-02-20 15:07:12 +1100

    feat: Testing a wrapper to execute SQL via data api sdk

--- aws/rdsdataapi/client.go
+++ aws/rdsdataapi/client.go
@@ -1,10 +1,18 @@
 package rdsdataapi
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rdsdataservice"
 )
+
+type AuroraRDSConfig struct {
+	database             *string
+	resourceArn          *string
+	secretArn            *string
+	rdsDataServiceClient *rdsdataservice.RDSDataService
+}
 
 func getNewSession(awsRegion *string) (*session.Session, error) {
 	session, err := session.NewSession(&aws.Config{
@@ -23,6 +31,22 @@ func GetNewClient(awsRegion *string) *rdsdataservice.RDSDataService {
 	rdsDataServiceClient := rdsdataservice.New(session)
 
 	return rdsDataServiceClient
+}
+
+func ExecuteSQL(rdsConfig *AuroraRDSConfig, sql *string) {
+	req, resp := rdsConfig.rdsDataServiceClient.ExecuteStatementRequest(&rdsdataservice.ExecuteStatementInput{
+		Database:    aws.String(*rdsConfig.database),
+		ResourceArn: aws.String(*rdsConfig.resourceArn),
+		SecretArn:   aws.String(*rdsConfig.secretArn),
+		Sql:         aws.String(*sql),
+	})
+
+	err1 := req.Send()
+	if err1 == nil { // resp is now filled
+		fmt.Println("Response:", resp)
+	} else {
+		fmt.Println("error:", err1)
+	}
 }
 
 func Heartbeat() string {
